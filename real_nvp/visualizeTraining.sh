c114ae98c918adaf39a8a30a8472f12978c76189
alexggmatthews <alexggmatthews@googlemail.com>
2017-01-24 12:25:09 +0000

    ImageNet visualization script from readme.

---
+++ real_nvp/visualizeTraining.sh
@@ -0,0 +1,8 @@
+python2.7 real_nvp_multiscale_dataset.py \
+--image_size 32 \
+--hpconfig=n_scale=4,base_dim=32,clip_gradient=100,residual_blocks=4 \
+--dataset imnet \
+--traindir /tmp/real_nvp_imnet32/train \
+--logdir /tmp/real_nvp_imnet32/sample \
+--data_path ../../small_imnet/valid_32x32_?????.tfrecords \
+--mode sample
